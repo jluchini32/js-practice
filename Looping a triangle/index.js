21301f6c5532fe7615ed5c14fd647ed7cc850534
joe-kasa <[joe@kasa.com]>
2022-01-09 21:01:11 -0700

    Looping a triange

---
+++ Looping a triangle/index.js
@@ -0,0 +1,15 @@
+//write a loops that mskes seven calls to console.log to output the following triangle
+/* 
+#
+##
+###
+####
+#####
+######
+####### */
+
+let hash = "#";
+while (hash.length <= 7){
+	console.log(hash)
+  hash = hash + "#"
+}
