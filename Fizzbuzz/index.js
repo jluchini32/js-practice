a9f047bb15fc26254e64fd6eca0afa097e0bd74b
joe-kasa <[joe@kasa.com]>
2022-01-09 22:44:20 -0700

    commit

--- Fizzbuzz/index.js
+++ Fizzbuzz/index.js
@@ -20,3 +20,12 @@ for (let number = 0; number <= 100; number = number +1 ){
  		console.log(number) 	
   }
 }
+
+
+// more simply 
+for (var i = 1; i < 101; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
